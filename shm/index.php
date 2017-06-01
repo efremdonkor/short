ac3df2719b2263dcf1fdd3774568c313ebaf2855
maxdonkor <maxdonkor@2012ACCPAC.0lqdrfqu5ihevinuhib1se51kh.bx.internal.cloudapp.net>
2017-06-01 08:55:32 -0400

    After first Page

---
+++ shm/index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>SHB :: The Best for the DMV event hosting</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        
+        <div class="jumbotron jumbotron-main">
+            <!-- NAVBAR  -->
+            <nav class="navbar">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapse" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">SMB</a>
+                    </div>
+                    <div id="navbar" class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li class="active"><a href="about.php">About</a></li>
+                            <li class="active"><a href="service.php">Services</a></li>
+                            <li class="active"><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- MAIN PAGE -->
+            <div class="container">
+                <h1>Short Handsome Boy</h1>
+                <p class="lead">
+                    The preferred host for all occasions in the DMV and beyond.
+                </p>
+                <p>
+                <a class="btn btn-primary btn-lg" href="about.php">Read More</a>
+                <a class="btn btn-default btn-lg" href="contact.php">Contact Us</a>
+                </p>
+            </div>
+        </div>
+        <?php
+        // put your code here
+        ?>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 box">
+                    <div class="well">
+                        <span class="glyphicon glyphicon-phone"></span>
+                        <h3>Dynamic Entertainer</h3>
+                        <p>Great personality for the utmost entertainment at any event. Be it adult or children</p>
+                    </div>
+                </div> 
+                <div class="col-md-4 box">
+                    <div class="well">
+                        <span class="glyphicon glyphicon-film"></span>
+                        <h3>Comedy for Attention</h3>
+                        <p>Great personality for the utmost entertainment at any event. Be it adult or children</p>
+                    </div>
+                </div> 
+                <div class="col-md-4 box">
+                    <div class="well">
+                        <span class="glyphicon glyphicon-search"></span>
+                        <h3>Full Engagement</h3>
+                        <p>Great personality for the utmost entertainment at any event. Be it adult or children</p>
+                    </div>
+                </div> 
+            </div>
+        </div>
+        
+        
+        <div class="section-a">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>QUOTES</h1>
+                    <p>This is a section for SHB funny/religious/spiritual event quotes</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img class="img-full" src="img/mobile.jpg">
+            </div>
+                 <div class="col-md-6">
+                     <h1>Reach us through all mediums</h1>
+                     <p>You can reach SHB from any communication system or application you might have. No need to worry about getting something new or sophisticated.</p>
+                     <ul class="list-group">
+                         <li class="list-group-item"> <span class="glyphicon glyphicon-apple">iPhone</span></li>
+                        <li class="list-group-item"> <span class="glyphicon glyphicon-apple">iPhone</span></li>
+                         <li class="list-group-item"> <span class="glyphicon glyphicon-apple">iPhone</span></li>
+                         <li class="list-group-item"> <span class="glyphicon glyphicon-apple">iPhone</span></li>
+                        <li class="list-group-item"> <span class="glyphicon glyphicon-apple">iPhone</span></li>
+                         <li class="list-group-item"> <span class="glyphicon glyphicon-apple">iPhone</span></li>
+                     </ul>
+            </div>
+        </div>
+            
+            <footer class="main-footer">
+                <div class="container">
+                    <div class="row">
+                        <p>
+                            Copyright &copy; Efrem Technologies, 2017
+                        </p>
+                    </div>
+                </div>
+            </footer>
+        
+        <script src="https://code.jquery.com/jquery-1.12.4.js" integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU=" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.js"></script>
+    </body>
+</html>
